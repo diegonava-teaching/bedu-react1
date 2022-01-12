c01feefe9e2abbb35a78d37856e45b9018e810b2
Diego Nava <diegomorenonava@gmail.com>
2022-01-11 21:59:49 -0600

    Added inline style example

--- src/components/Foco/index.js
+++ src/components/Foco/index.js
@@ -2,7 +2,16 @@ import Luz from '../Luz';
 
 function Foco() {
   return (
-    <div className='foco'>
+    <div
+      style={{
+        width: 100,
+        height: 100,
+        backgroundColor: 'gray',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-around',
+      }}
+    >
       <Luz />
     </div>
   );
